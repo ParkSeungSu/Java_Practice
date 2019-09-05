504ee59b4ce4b63c1c845de9235e9e1558d6772a
ParkSeungSu <cldkr0401@naver.com>
2019-09-05 16:44:43 +0900

    20190905
    practice

--- helloworld/src/main/java/kr/ac/halla/h02404/helloworld/MyJSONHelloWorld.java
+++ helloworld/src/main/java/kr/ac/halla/h02404/helloworld/MyJSONHelloWorld.java
@@ -7,8 +7,11 @@ public class MyJSONHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JSONObject jsonHello=new JSONObject();
+		JSONObject jsonYallo=new JSONObject();
 		jsonHello.put("message","My JSON Hello World!");
+		jsonYallo.put("number",12412);
 		System.out.println(jsonHello);
+		System.out.println(jsonYallo);
 	}
 
 }
